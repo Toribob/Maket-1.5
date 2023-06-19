54c095cec91c60b28e560804d89ac1685ce22cac
Toribob <Toribosh@yandex.ru>
2023-06-20 01:37:51 +0300

    Maket 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
 let readMoreButton = document.querySelector('.expand-button');
 let readMoreButtonText = readMoreButton.querySelector('span');
 let readMoreButtonImg = readMoreButton.querySelector('img');
-let brandList = document.querySelector('.swiper-wrapper');
+let brandList = document.querySelector('.brand-list');
 
 readMoreButton.addEventListener('click', function () {
     if (readMoreButton.classList.contains('expand-button--nonactive')) {
         readMoreButton.classList.remove('expand-button--nonactive');
-        brandList.classList.remove('swiper-wrapper--hidden');
+        brandList.classList.remove('brand-list--hidden');
         readMoreButtonImg.src = 'img/icons/Hide.svg';
         readMoreButtonText.textContent = "Скрыть";
     } else {
         readMoreButton.classList.add('expand-button--nonactive');
-        brandList.classList.add('swiper-wrapper--hidden');
+        brandList.classList.add('brand-list--hidden');
         readMoreButtonImg.src = 'img/icons/read-more.svg';
         readMoreButtonText.textContent = "Показать все";
     }
     
 });
 
-new Swiper('.brand-list');
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width < 321){
+        new Swiper('.brand-list',{
+            slidesPerView: 1.3,
+            spaceBetween: 16,
+            pagination: {
+                el: ".swiper-pagination",
+              },
+        });
+    };
+});
+
+
 
 
 
